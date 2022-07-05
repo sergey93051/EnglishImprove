195085880ffc06bac631ccbc3445cf914ad90d2c
Grigor Tonoyan <grigor.tonoyan96@gmail.com>
2022-07-05 14:35:21 +0400

    Add new logic for change password

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -12,14 +12,17 @@ Route::get('verify_account/{hash}',
 
 Route::middleware('auth:sanctum')->group(function (){
     Route::post('logout', \App\Http\Controllers\Api\V1\Auth\LogoutController::class);
-
+    Route::post('change-password', \App\Http\Controllers\Api\V1\Auth\ChangePasswordController::class);
+    
     Route::post('verify_account' ,[\App\Http\Controllers\Api\V1\Auth\VerifyAccountController::class,
         'sendResetToken']);
 
     Route::get('get-information',[\App\Http\Controllers\Api\V1\UserInformationController::class,'get']);
     Route::put('update-information',[\App\Http\Controllers\Api\V1\UserInformationController::class,'updatePersonalInformation']);
 
-    Route::get('check-verified',[\App\Http\Controllers\Api\V1\Auth\VerifyAccountController::class,'checkVerified']); });
+    Route::get('check-verified',[\App\Http\Controllers\Api\V1\Auth\VerifyAccountController::class,'checkVerified']);
+
+     });
 
 
 
